941e457ff34fb5ab3be68cf1dace9d60fdf31d49
shubhammedewar <shubhammedewar@gmail.com>
2025-07-24 11:49:40 +0530

    Marks Criteria

---
+++ compareMarks.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class compareMarks {
+    public static void main(String [] arg){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+        if(marks > 60 && marks <=70)
+            System.out.println("Passed");
+        else if(marks > 70 && marks <=80)
+            System.out.println("Passed with grade 'C' ");
+        
+        else if(marks > 80 && marks <=90)
+            System.out.println("Passed with grade 'B' ");
+        else if (marks >90 )
+            System.out.println("passed with grade 'A' ");
+        
+        else
+            System.out.println("failed!!!");
+    }
+}

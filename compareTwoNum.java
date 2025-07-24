486dadc46e1e97a30f34107d5e6a684407f8f3c9
shubhammedewar <shubhammedewar@gmail.com>
2025-07-24 11:13:25 +0530

    Comparison of Two numbers

---
+++ compareTwoNum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class compareTwoNum{
+    public static void main(String[] arg){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int a = sc.nextInt();
+        System.out.print("enter value: ");
+        int b = sc.nextInt();
+
+        if(a>b){
+            System.out.println(a + " is greater than "+b);
+        }
+        else System.out.print(b +" is greater than " +a);
+
+        sc.close();
+    }
+}

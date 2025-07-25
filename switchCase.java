8f40c391ad91352a8494a91f6ce9a504c8eb712d
shubhammedewar <shubhammedewar@gmail.com>
2025-07-25 10:25:48 +0530

    Switch case example

---
+++ switchCase.java
@@ -0,0 +1,20 @@
+class switchCase{
+    public static void main(String [] arg){
+        Scanner sc = new Scanner(System.in);
+        int a  = sc.nextInt();
+        switch(a){
+            case 1:
+                System.out.println("This is case 1");
+                break;
+            case 2:
+                System.out.println("This is case 2");
+                break;
+            case 3:
+                System.out.println("This is case 3");
+                break;
+            default:
+                System.out.println("This is default");
+        }
+        sc.close();
+    }
+}
